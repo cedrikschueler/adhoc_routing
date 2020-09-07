49c29d7b3578e1004669f2dd02fe3c9ce776f4ca
cedrikschueler <cedrik.schueler@tu-dortmund.de>
2020-09-07 15:41:47 +0200

    Add Subscriptions

--- Routing/UDPManager.py
+++ Routing/UDPManager.py
@@ -3,15 +3,21 @@ import threading
 import time
 
 ENCODING = "utf-8"
+
 class UDPManager(threading.Thread):
 
-    def __init__(self, port: int) -> None:
+    def __init__(self, port: int, buffersize=10000) -> None:
         threading.Thread.__init__(self)
         self.address = ""
         self.port = port
+        self.bufferSize = buffersize
 
+        self.subscriptions = []
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+    def subscribe(self, callable):
+        self.subscriptions.append(callable)
 
     def listen(self) -> None:
         self.socket.bind((self.address,  self.port))
@@ -20,13 +26,16 @@ class UDPManager(threading.Thread):
     def run(self):
         try:
             while True:
-                data = self.socket.recv(1024)   #todo buffer size
-                print("received message: %s" % data.decode(ENCODING))
+                data, addr = self.socket.recvfrom(self.bufferSize)
+                if len(self.subscriptions) != 0:
+                    for c in self.subscriptions:
+                        c(data, addr[0])
+                print("received message with ", len(data), " Bytes from ", addr)
         except StopIteration:
             pass
 
-    def broadcastData(self, data: str):
-        self.socket.sendto(data.encode(ENCODING), ('<broadcast>', self.port))
+    def broadcastData(self, data):
+        self.socket.sendto(data, ('<broadcast>', self.port))
 
 if __name__ == "__main__":
     udp_mgr = UDPManager(1801)
