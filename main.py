d8b7c23bce860ce6b6eeead94c88e7ce9f77f2ce
cedrikschueler <cedrik.schueler@tu-dortmund.de>
2020-09-08 12:43:57 +0200

    Define test scenario

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-def main():
-    raise Exception("Not implemented yet.")
+from Routing.Parrod import Parrod
+import time
+
+def test_Experiment():
+    config = dict()
+    config["mhChirpInterval"] = 0.5
+    config["neighborReliabilityTimeout"] = 2.5
+    config["qFctAlpha"] = 0.4
+    config["qFctGamma"] = 0.9
+    config["maxHops"] = 32
+    config["historySize"]  =5
+    config["rescheduleRoutesOnTimeout"] = True
+    config["predictionMethod"] = "slope"
+    config["ipAddress"] = "192.168.178.27"
+
+
+    config["bcPort"] = 1801
+    config["bufferSize"] = 1460
+
+    config["gnssUpdateInterval"] = 0.2
+
+    parrod = Parrod(config, 230.0)
+    parrod.start()
+    time.sleep(5)
+    parrod.stop()
 
 
 if __name__ == "__main__":
-    main()
+    test_Experiment()
 
 
