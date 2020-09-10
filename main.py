eda4f105eaf831e3ede100a2cf4308b574cf4f2b
cedrikschueler <cedrik.schueler@tu-dortmund.de>
2020-09-10 14:04:20 +0200

    Extend Experiment setup

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from Routing.Parrod import Parrod
+from Traffic.TrafficGenerator import TrafficGenerator, TrafficReceiver
 import time
 
-def Experiment():
+def Experiment(timeLimit: float=120.0, waitTimeBeforeStart=0.0, trafficDelay=0.0):
+    routingProtocol = None
+    trafficInstance = None
+
+    """
+    Parametrize Routing Protocol
+    """
     config = dict()
     config["mhChirpInterval"] = 0.5
     config["neighborReliabilityTimeout"] = 2.5
@@ -18,15 +25,46 @@ def Experiment():
     config["bcPort"] = 1801
     config["bufferSize"] = 1460
 
-    config["gnssUpdateInterval"] = 0.2
+    config["gnssUpdateInterval"] = 1.0
 
-    parrod = Parrod(config, 230.0)
-    parrod.start()
-    time.sleep(120)
-    parrod.stop()
+    """
+    Parametrize Traffic
+    """
+    traffic = {
+        "CBR": 2.0,
+        "MTU": 1460,
+        "Sender": "192.168.178.27",
+        "Destination": "192.168.178.24",
+        "Port": 1901
+    }
+
+    if config["ipAddress"] == traffic["Sender"]:
+        trafficInstance = TrafficGenerator(traffic["CBR"], traffic["Destination"], MTU_used_byte=traffic["MTU"], port=traffic["Port"])
+    elif config["ipAddress"] == traffic["Destination"]:
+        trafficInstance = TrafficReceiver(traffic["Sender"], port=traffic["Port"], bufferSize=traffic["MTU"])
+
+    routingProtocol = Parrod(config, 230.0)
+
+    # After everything is set up, wait to start the experiment
+    time.sleep(waitTimeBeforeStart)
+
+    # Start routing instance and traffic instance if there is one
+    routingProtocol.start()
+    if trafficInstance is not None:
+        # Wait with traffic instance for given delay
+        time.sleep(trafficDelay)
+        trafficInstance.start()
+
+    # Let the experiment run before terminating it
+    time.sleep(timeLimit)
+
+    # Terminate instances
+    if trafficInstance is not None:
+        trafficInstance.terminate()
+    routingProtocol.terminate()
 
 
 if __name__ == "__main__":
-    Experiment()
+    Experiment(timeLimit=300, waitTimeBeforeStart=10.0, trafficDelay=5.0)
 
 
